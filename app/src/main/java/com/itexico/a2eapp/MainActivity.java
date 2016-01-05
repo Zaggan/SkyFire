5dbeb5b650f034e25f84a26c59c402dc24509f7c
Giovani Gonzalez <darkgeat@gmail.com>
2016-01-05 09:58:09 -0600

    Splash screen and also the demo code implemented and running

--- app/src/main/java/com/itexico/a2eapp/MainActivity.java
+++ app/src/main/java/com/itexico/a2eapp/MainActivity.java
@@ -1,13 +1,67 @@
 package com.itexico.a2eapp;
 
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare variables
+    ProgressDialog pDialog;
+    VideoView videoview;
+
+    // Insert your Video URL
+    String VideoURL = "http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+    //String VideoURL = "https://fish.schou.me/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
         setContentView(R.layout.activity_main);
+        // Find your VideoView in your video_main.xml layout
+        videoview = (VideoView) findViewById(R.id.myVideo);
+        // Execute StreamVideo AsyncTask
+
+        // Create a progressbar
+        pDialog = new ProgressDialog(MainActivity.this);
+        // Set progressbar title
+        pDialog.setTitle("Android Video Streaming Tutorial");
+        // Set progressbar message
+        pDialog.setMessage("Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(
+                    MainActivity.this);
+            mediacontroller.setAnchorView(videoview);
+            // Get the URL from String VideoURL
+            Uri video = Uri.parse(VideoURL);
+            videoview.setMediaController(mediacontroller);
+            videoview.setVideoURI(video);
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/itexico/a2eapp/Splash.java
@@ -0,0 +1,33 @@
+package com.itexico.a2eapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by darkgeat on 1/5/16.
+ */
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(Splash.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
